3bc81a1efb4cdbb5dc8f5a99f8c3585eab397b9d
theScrlLkKey <microman5252@gmail.com>
2022-06-13 18:27:57 -0700

    fix daft issue, prepare for multithreaded chat

--- Chatpy_windows.py
+++ Chatpy_windows.py
@@ -657,8 +657,8 @@ while True:
                                 print(f'All posts, sorted old to new: \n{newline.join(reversed(message))}')
                             else:
                                 print(f'''{print_time_str} |Private message from {username}{sep} {', '.join(message)}''')
-                                if getWindow() == curhwnd:
-                                    plyer.platforms.win.notification.instance().notify(title=f'Private message from: {username} | Chatpy', message=message, timeout=3)
+                                if not getWindow() == curhwnd:
+                                    plyer.platforms.win.notification.instance().notify(title=f'Private message from: {username} | Chatpy', message=', '.join(message), timeout=3)
                     except:
                         continue
 
@@ -671,7 +671,7 @@ while True:
                 elif f'@{my_username}' in message:
                     print(f'{print_time_str} |{username}{sep} {message}')
                     try:
-                        if getWindow() == curhwnd:
+                        if not getWindow() == curhwnd:
                             plyer.platforms.win.notification.instance().notify(title=f'{username} | Chatpy', message=message, timeout=3)
                         # toaster.show_toast(f'{username} | Chatpy', message, icon_path=None, duration=3, threaded=False)
                     except Exception as e:
